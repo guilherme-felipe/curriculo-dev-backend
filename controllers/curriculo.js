b5a6826127f0e6ad920bf2ea53867f0e1396c98b
Guilherme Felipe <guilhermefelipesimao@gmail.com>
2021-09-06 11:28:14 -0300

    aula 7

--- controllers/curriculo.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-  const dataDB = app.data.dados;
-  const controller = {};
-
-  controller.listDataDB = (req, res) => res.status(200).json(dataDB);
-
-  return controller;
-};
--- routes/curriculoRoute.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) => {
-  const controller = app.controllers.curriculo;
-
-  app.route("/api/v1/data-curriculo").get(controller.listDataDB);
-};
