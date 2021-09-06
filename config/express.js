828b9878355930bd7ffeee2e81940e8344c25b7d
Guilherme Felipe <guilhermefelipesimao@gmail.com>
2021-09-06 10:42:36 -0300

    deploy

---
+++ config/express.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const config = require("config");
+const consign = require("consign");
+
+module.exports = () => {
+  const app = express();
+
+  app.set("port", process.env.PORT || config.get("server.port"));
+
+  app.use(express.json());
+
+  consign({ cwd: "api" })
+    .then("../data")
+    .then("../controllers")
+    .then("../routes")
+    .into(app);
+
+  return app;
+};
