5d604ee5fe8b98b11dddf127e379317d681983f0
Guilherme Felipe <guilhermefelipesimao@gmail.com>
2021-09-12 13:41:21 -0300

    FIX CORS

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const consign = require("consign");
+const cors = require("cors");
 
 module.exports = () => {
   const app = express();
@@ -8,6 +9,7 @@ module.exports = () => {
   app.set("port", process.env.PORT || config.get("server.port"));
 
   app.use(express.json());
+  app.use(cors()); // <---- use cors middleware
 
   consign().then("data").then("controllers").then("routes").into(app);
 
